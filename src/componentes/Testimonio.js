3f1463eed998e543e9a521a0a387ce665e21af73
JBriceno <josealbertino4@gmail.com>
2022-06-26 10:03:36 -0500

    terminado el proyecto, falta hacerlo responsive, y hacer un for para publicar el componente mas facilmente

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,23 +1,26 @@
 import React from "react";
+import "../hojas-de-estilo/Testimonio.css";
 
-function Testimonio() {
-    return (
-        <div className="contenedor-testimonio">
-            <img className="imagen-testimonio"
-            src={require("../imagenes/testimonio-emma.png")}
-            alt="Imagen de emma" />
-        
-        <div className="contenedor-texto-testimonio">
-            <p className="nombre-testimonio">
-            Emma Bostian en Suecia    </p>
-            <p className="cargo-testimonio">
-            Ingeniera de Software en Spotify   </p> 
-            <p className="texto-testimonio">
-             "Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."   </p>  
+function Testimonio(props) {
+  return (
+    <div className="contenedor-testimonio">
+      <img
+        className="imagen-testimonio"
+        src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+        alt="Imagen de emma"
+      />
 
-        </div>
-        </div>
-    );
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">
+          <strong>{props.nombre}</strong> en {props.pais}
+        </p>
+        <p className="cargo-testimonio">
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p className="texto-testimonio">"{props.testimonio}"</p>
+      </div>
+    </div>
+  );
 }
 
 export default Testimonio;
