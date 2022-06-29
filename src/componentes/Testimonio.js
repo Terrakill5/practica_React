436add044c8c783a071a85fafe45dae737a846f6
JBriceno <josealbertino4@gmail.com>
2022-06-28 23:09:53 -0500

    agregado tailwind en CDN

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -3,21 +3,21 @@ import "../hojas-de-estilo/Testimonio.css";
 
 function Testimonio(props) {
   return (
-    <div className="contenedor-testimonio">
+    <div className="flex items-center justify-center bg-gris shadow-asi mx-2.5 mt-2.5 mb-12 h-96">
       <img
-        className="imagen-testimonio"
+        className="w-auto h-full"
         src={require(`../imagenes/testimonio-${props.imagen}.png`)}
         alt={`Imagen de ${props.imagen}`}
       />
 
-      <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio">
+      <div className="contenedor-texto-testimonio text-center p-2.5 font-normal leading-6">
+        <p className="text-xl">
           <strong>{props.nombre}</strong> en {props.pais}
         </p>
-        <p className="cargo-testimonio">
+        <p className="text-xl mt-4 pb-8">
           {props.cargo} en <strong>{props.empresa}</strong>
         </p>
-        <p className="texto-testimonio">"{props.testimonio}"</p>
+        <p className="text-lg text-justify">"{props.testimonio}"</p>
       </div>
     </div>
   );
